de6289a133d0a8bf572e468cb3de6bbb9a00446b
Nick Mckloski <Nick@50.156.190.116>
2015-11-17 16:12:57 -0500

    init commit, project restructure

---
+++ android/src/com/nickm/rpg/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.nickm.rpg.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.nickm.rpg.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
